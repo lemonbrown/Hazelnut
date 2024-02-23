0200a4a85574665cc82c337646d65aeb3cd06b0a
cambriner <cameron@yourcase.works>
2024-02-23 16:33:21 -0600

    db ignore

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,19 @@
 // @deno-types="npm:@types/express@4"
 import express, { NextFunction, Request, Response } from "npm:express@4.18.2";
 
+import { DB } from "https://deno.land/x/sqlite/mod.ts";
+
+const db = new DB("test.db");
+
+
 const app = express();
 const port = Number(Deno.env.get("PORT")) || 3000;
 
 app.get("/", (_req, res) => {
     res.status(200).send("Hello from Deno and Express!");
-  });
+});
 
-  app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Listening on ${port} ...`);
-  });
+});
+
