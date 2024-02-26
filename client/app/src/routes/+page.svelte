40422265aaaa017d896e059b9e34f97ba0094a0b
cambr <cambriner@gmail.com>
2024-02-26 14:07:43 -0600

    asdfadsf

---
+++ client/app/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<div class="columns is-mobile">
+    <div class="column is-half is-offset-one-quarter">hi</div>
+  </div>
---
+++ client/app/src/routes/register/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	export let data;
+</script>
+
+<div class="container">
+    <div class="columns is-centered">
+        <div class="column is-half">
+            <div class="block">
+                <span class="title">Register</span>
+            </div>
+            <form class="" method="POST">
+                <div class="field">
+                  <label class="label">Email</label>
+                  <div class="control">
+                    <input class="input" type="email" placeholder="e.g. alex@example.com" name="email">
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Password</label>
+                  <div class="control">
+                    <input class="input" type="password" placeholder="********" name="password">
+                  </div>
+                </div>
+                <div class="field">
+                    <label class="label">Confirm Password</label>
+                    <div class="control">
+                      <input class="input" type="password" placeholder="********" name="confirmPassword">
+                    </div>
+                  </div>
+                <button class="button" type="submit">Register</button>
+              </form>
+        </div>
+    </div>
+</div>
